a584edf192aafe3e2df1cb22af8d1e282529c8f1
komei-work-work <koba.work,work1127@gmail.com>
2020-04-22 00:24:04 +0900

    Affine transform for tensorflow

--- src/images/tf_affine.py
+++ src/images/tf_affine.py
@@ -9,10 +9,11 @@ def base_transform_map(size, dtype="float32"):
     :param Any dtype:
     :return:
     """
+
     height, width = size
     x = np.tile(np.linspace(0, width, width).reshape(1, -1), (height, 1))
     y = np.tile(np.linspace(0, height, height).reshape(-1, 1), (1, width))
-    return tf.constant([[x, y, np.ones(x.shape)]], dtype)
+    return tf.constant(np.array([[x, y, np.ones(x.shape)]]), dtype)
 
 
 def transform(image, base_map, affine_matrix):
@@ -24,10 +25,14 @@ def transform(image, base_map, affine_matrix):
     :return:
     """
     height, width = image.shape[:2]
-    mat = np.linalg.inv(affine_matrix)[..., None, None]
-    dx, dy, _ = np.sum(base_map * mat, axis=1)
-    x_index = tf.clip_by_value(dx, 0, width - 1).astype('i')
-    y_index = tf.clip_by_value(dy, 0, height - 1).astype('i')
+    mat = tf.matrix_inverse(affine_matrix)[..., None, None]
+    indices = tf.reduce_sum(base_map * mat, axis=1)
+    dx = indices[0]
+    dy = indices[1]
+    x_index = tf.cast(tf.clip_by_value(dx, 0, np.int32(width) - 1), "int32")
+    y_index = tf.cast(tf.clip_by_value(dy, 0, np.int32(height) - 1), "int32")
+    print(x_index)
+    print(y_index)
     return image[y_index, x_index]
 
 
@@ -38,7 +43,7 @@ def translation_matrix(delta):
     :return:
     """
     dx, dy = delta
-    return tf.constant([
+    return tf.stack([
         [1, 0, dx],
         [0, 1, dy],
         [0, 0, 1]
@@ -55,7 +60,7 @@ def rotation_matrix(radian, anchor=(0, 0)):
     ax, ay = anchor
     sin = tf.math.sin(radian)
     cos = tf.math.cos(radian)
-    return tf.constant([
+    return tf.stack([
         [cos, -sin, ax - ax * cos + ay * sin],
         [sin, cos,  ay - ax * sin - ay * cos],
         [0,   0,    1]
@@ -63,7 +68,7 @@ def rotation_matrix(radian, anchor=(0, 0)):
 
 
 def shear_matrix(mx, my):
-    return tf.constant([
+    return tf.stack([
         [1,   -mx, 0],
         [-my, 1,   0],
         [0,   0,   1]
@@ -79,7 +84,7 @@ def zoom_matrix(zoom, anchor=(0, 0)):
     """
     zx, zy = zoom
     ax, ay = anchor
-    return tf.constant([
+    return tf.stack([
         [zx, 0,      -ax * zx + ax],
         [0,      zy, -ay * zy + ay],
         [0,      0,  1]
